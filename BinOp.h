e9a3052557aa65c7bbf541e0c509be3d791a0a5f
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-08-21 13:54:08 -0600

    binops 1

--- BinOp.h
+++ BinOp.h
@@ -1,16 +1,36 @@
 #ifndef __BINOP_H__
 #define __BINOP_H__
 
+#include <inttypes.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include "random.h"
 
 // NIST FIPS 203; Algoritm 3
-int* BitsToBytes(int* b);
+uint8_t* BitsToBytes(int* b, int l){
+    uint8_t* B = (uint8_t*)malloc(l*sizeof(uint8_t));
+    for (int i = 0; i<8*l; i++){
+        B[(int)(i/8)]=B[(int)(i/8)]+b[i]*pow(2, i%8);
+    }
+    return B;
+}
 
 // NIST FIPS 203; Algoritm 4
-int* BytesToBits(int* B);
+int* BytesToBits(uint8_t* B, int l){
+    uint8_t* C = (uint8_t*)malloc(l*sizeof(uint8_t));
+    int* b = (int*)malloc((8*l)*sizeof(int));
+    for (int i =0; i<l;i++){
+        for (int j=0; j<8; j++){
+            b[8*i+j]=(int)(C[i]%2);
+            C[i]=(uint8_t)(C[i]/2);
+        }
+    }
+    return b;
+}
 
 // NIST FIPS 203; Algoritm 5
-int* ByteEncode(int* F, int d);
+uint8_t* ByteEncode(int* F, int d);
 
 // NIST FIPS 203; Algoritm 6
 int* ByteDecode(int* B, int d);
